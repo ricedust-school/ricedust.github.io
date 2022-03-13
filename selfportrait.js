a0ab8549e55ba0819124b711ea41ce7883254a8b
Shane Nguyen <sn103103@gmail.com>
2022-03-12 23:41:26 -0800

    updated self portrait

--- selfportrait.js
+++ selfportrait.js
@@ -1,13 +1,17 @@
-function preload() {
-  // img = loadImage("ref.png");  
-}
-
 function setup() {
   let cnv = createCanvas(600, 600);
   let newCanvasX = (windowWidth - 600)/2;
   let newCanvasY = (windowHeight - 600)/2;
   cnv.position(newCanvasX, newCanvasY);
   strokeCap(SQUARE);
+  
+  // calls drawing functions
+  background(255);
+  black();
+  shadow();
+  midtone();
+  highlight();
+  // guide();
 }
 
 // sets pencil settings
@@ -16,8 +20,24 @@ function pencil() {
   stroke(0);
 }
 
-// contains instructions to sketch with pencil
-function sketch() {
+// sets charcoal settings
+function charcoal(tone) {
+  if (tone === 'black') {
+    stroke(0, 150);
+  }
+  else if (tone === 'shadow') {
+    stroke(0, 75);  
+  }
+  else if (tone === 'midtone') {
+    stroke(0, 30); 
+  }
+  else if (tone === 'highlight') {
+    stroke(255, 125);
+  }
+}
+
+// contains instructions to create guide with pencil
+function guide() {
   pencil();
   // face outline
   line(185, 275, 215, 425);
@@ -201,51 +221,244 @@ function sketch() {
   line(177, 155, 199, 223);
 }
 
-// sets charcoal settings
-function charcoal(tone) {
-  strokeWeight(30);
-  if (tone === 'shadow') {
-    stroke(0, 50);  
-  }
+// contains instructionss to add blacks
+function black() {
+  charcoal('black');
+  strokeWeight(50);
+  
+  // hair
+  line(409, 79, 465, 239);  
+  line(467, 200, 432, 270);
+  line(423, 91, 410, 245);
+  line(210, 89, 338, 65);
+  line(307, 60, 380, 131);
+  line(350, 246, 419, 195);
+  line(279, 182, 307, 256);
+  line(276, 196, 315, 98);
+  line(314, 265, 323, 122);
+  line(361, 265, 373, 113);
+  line(273, 232, 415, 146);
+  line(365, 84, 333, 181);
+  line(195, 245, 169, 180);
+  line(241, 69, 337, 87);
+  
+  // shirt
+  line(402, 541, 493, 580);
+  line(486, 575, 524, 615);
+  line(431, 530, 400, 615);
+  line(385, 582, 474, 620);
+  line(446, 557, 519, 610);
+  line(181, 556, 170, 556);
+  line(129, 583, 120, 589);
+  
+  strokeWeight(25);
+  
+  // hair
+  line(302, 105, 265, 192);
+  line(232, 147, 306, 70);
+  line(296, 250, 368, 251);
+  line(464, 265, 403, 225);
+  line(471, 215, 437, 118);
+  line(193, 94, 167, 135);
+  line(167, 125, 162, 193);
+  line(229, 63, 212, 119);
+  
+  // nostrils
+  //line(329, 394, 328, 399);
+  //line(370, 391, 373, 396);
+  
+  // iris
+  line(279, 288, 277, 308);
+  line(400, 306, 398, 285);
+  
+  // shirt
+  line(397, 514, 406, 549);
+  line(179, 522, 259, 605);
+  line(149, 546, 180, 590);
+  line(102, 570, 110, 599);
 }
 
-// contains instructions to block in shadows
+// contains instructions to add shadows
 function shadow() {
   charcoal('shadow');
+  strokeWeight(50);
+  
   // face
-  line(357, 246, 384, 403);
-  line(375, 377, 360, 419);
-  line(373, 414, 371, 506);
-  line(406, 405, 347, 507);
-  line(405, 251, 358, 318);
-  line(425, 342, 405, 412);
-  line(427, 259, 425, 350);
-  line(374, 456, 414, 232);
-  line(363, 301, 379, 370);
-  line(388, 247, 406, 437);
-  line(420, 268, 424, 374);
-  line(420, 364, 380, 455);
+  line(420, 255, 415, 355);
+  line(369, 249, 384, 366);
+  line(413, 253, 366, 312);
+  line(396, 323, 403, 400);
+  line(401, 397, 340, 506);
+  line(348, 268, 437, 232);
+  line(418, 343, 392, 415);
+  
+  // eyes
+  line(255, 290, 256, 294);
+  line(412, 286, 411, 290);
+  
+  // ears
+  line(184, 317, 178, 319);
+  
+  // hair
+  line(332, 55, 430, 116);
+  line(221, 73, 190, 185);
+  line(221, 243, 207, 137);
+  line(243, 253, 290, 227);
+  line(167, 201, 280, 221);  
+  line(289, 103, 219, 196);
+  
+  // lips
+  line(346, 441, 347, 449);
+  line(367, 436, 368, 439);
+  line(325, 441, 325, 444);
   
   // neck
-  line(325, 513, 326, 599);
-  line(307, 549, 326, 599);
-  line(306, 560, 328, 513);
-  line(364, 508, 391, 542);
-  line(350, 508, 353, 544);
-  line(391, 558, 341, 527);
-  line(382, 509, 363, 561);
+  line(421, 596, 352, 502);
+  line(335, 510, 333, 599);
+  line(337, 527, 395, 542);
+  line(354, 566, 363, 566);
   
-}
+  // shirt
+  line(200, 532, 34, 630);
+  line(150, 532, 280, 620);
+  line(214, 590, 107, 599);
+  line(230, 577, 97, 615);
+  line(125, 549, 165, 599);
+  line(69, 586, 92, 620);
+  
+  strokeWeight(25);
+  
+  // face
+  line(365, 406, 377, 361);
+  line(360, 301, 380, 388);
+  line(300, 406, 290, 369);
+  line(414, 395, 357, 497);
+  
+  // eyes
+  line(291, 290, 289, 294);
+  line(303, 300, 298, 302);
+  line(381, 294, 385, 297);
+  line(412, 302, 416, 309);
+  
+  // ears
+  line(167, 291, 175, 315);
+  line(160, 274, 154, 274);
+  
+  // hair
+  line(199, 142, 263, 94);
+  line(286, 112, 207, 212);
+  line(243, 248, 260, 131);
+  line(334, 46, 420, 100);
+ 
+  // nose
+  line(360, 386, 370, 422);
+  line(328, 386, 327, 397);
+  
+  // philtrum
+  line(349, 417, 342, 417);
+  
+  // lips
+  line(368, 429, 367, 437);
+  line(371, 447, 362, 436);
+  line(322, 433, 329, 443);
+  line(323, 433, 324, 436);
+  
+  // neck
+  line(316, 557, 324, 610);
+  line(323, 516, 305, 561);
+  
+  // shirt
+  line(176, 514, 207, 568);
+  line(157, 532, 283, 610);
+  }
 
-function highlight() {
+// contains instructions to add midtones
+function midtone() {
+  charcoal('midtone');
+  strokeWeight(50);
+  
+  // face
+  line(335, 510, 327, 261);
+  line(231, 408, 321, 488);
+  line(310, 438, 266, 243);
+  line(213, 228, 260, 420);
+  line(241, 277, 320, 305);
+  line(340, 508, 354, 399);
+  line(354, 393, 355, 407);
+  line(356, 462, 357, 469);
+  line(271, 357, 316, 258);
+  line(442, 299, 382, 301);
     
+  // ear
+  line(170, 250, 194, 354);
+  line(184, 273, 179, 277);
+  
+  // lips
+  line(333, 430, 332, 454);
+  
+  // neck
+  line(220, 466, 383, 596);
+  line(204, 490, 300, 597);
+  line(347, 512, 374, 599);
+  
+  strokeWeight(25);
+  
+  // face
+  line(192, 237, 225, 423);
+  line(346, 264, 363, 381);
+  line(306, 432, 304, 455);
+  
+  // eyes
+  line(288, 310, 290, 317);
+  
+  // ears
+  line(190, 345, 185, 342);
+  line(189, 352, 194, 359);
+  line(166, 280, 172, 282);
+  
+  // lips
+  line(336, 430, 332, 444);
+  line(299, 457, 319, 444);
+  line(319, 451, 323, 440);
+  line(320, 432, 325, 442);
 }
 
-function draw() {
-  // image(img, 0, 0);
-  background(255);
-  shadow();
-  sketch();
-  strokeWeight(1);
-  text(mouseX + ", " + mouseY, mouseX, mouseY);
+// contains instructions to add highlights
+function highlight() {
+  charcoal('highlight'); 
+  strokeWeight(50);
+  
+  // face
+  line(229, 245, 212, 290);
+  line(244, 347, 210, 282);
+  line(213, 265, 215, 309);
+  
+  // nose
+  line(330, 373, 331, 381);
+  
+  // neck
+  line(201, 530, 242, 455);
+  line(219, 469, 277, 577);
+  line(365, 564, 350, 592);
+      
+  strokeWeight(25);
+  
+  // face
+  line(315, 399, 320, 482);
+  line(316, 474, 327, 492);
+  
+  // nose
+  line(318, 373, 325, 326);
+  
+  // philtrum
+  line(354, 414, 349, 415);
+  
+  // lips
+  line(324, 447, 323, 452);
+  line(348, 449, 348, 454);
+  line(317, 428, 318, 431);
+  
+  // ears
+  line(172, 262, 173, 288);
+  line(176, 321, 191, 355);
 }
